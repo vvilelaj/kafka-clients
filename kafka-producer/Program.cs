0abfa9e830df70be91aa75677f058b0d55acea74
Victor Vilela <victor.vilela@outlook.com>
2021-03-07 17:51:28 -0500

    update producer

---
+++ kafka-producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace kafka_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = GetConfig();
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                int numProduced = 0;
+                int numMessages = 10;
+                string topic = "first_topic";
+
+                for (int i = 0; i < numMessages; ++i)
+                {
+                    var key = "alice";
+                    var val = JObject.FromObject(new { count = i }).ToString(Formatting.None);
+
+                    Console.WriteLine($"Producing record: {key} {val}");
+
+                    producer.Produce(topic, new Message<string, string> { Key = key, Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+
+        private static ClientConfig GetConfig()
+        {
+            var config = new ClientConfig();
+            config.BootstrapServers = "localhost:9092";
+            return config;
+        }
+    }
+}
