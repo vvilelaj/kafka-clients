f51337b3662fe459a2a4c8fce33865017cc6e08c
Victor Vilela <victor.vilela@outlook.com>
2021-03-07 19:43:32 -0500

    add consumer

--- README.md
+++ README.md
@@ -22,16 +22,16 @@ Producing record: alice {"count":6}
 Producing record: alice {"count":7}
 Producing record: alice {"count":8}
 Producing record: alice {"count":9}
+Produced message to: first_topic [[0]] @0
+Produced message to: first_topic [[0]] @1
+Produced message to: first_topic [[0]] @2
+Produced message to: first_topic [[0]] @3
 Produced message to: first_topic [[0]] @4
 Produced message to: first_topic [[0]] @5
 Produced message to: first_topic [[0]] @6
 Produced message to: first_topic [[0]] @7
 Produced message to: first_topic [[0]] @8
 Produced message to: first_topic [[0]] @9
-Produced message to: first_topic [[0]] @10
-Produced message to: first_topic [[0]] @11
-Produced message to: first_topic [[0]] @12
-Produced message to: first_topic [[0]] @13
 10 messages were produced to topic first_topic
 
 ```
@@ -49,5 +49,15 @@ dotnet run
 
 ### output
 ```
+Consumed record with key alice and value {"count":0}, and updated total count to 0
+Consumed record with key alice and value {"count":1}, and updated total count to 1
+Consumed record with key alice and value {"count":2}, and updated total count to 3
+Consumed record with key alice and value {"count":3}, and updated total count to 6
+Consumed record with key alice and value {"count":4}, and updated total count to 10
+Consumed record with key alice and value {"count":5}, and updated total count to 15
+Consumed record with key alice and value {"count":6}, and updated total count to 21
+Consumed record with key alice and value {"count":7}, and updated total count to 28
+Consumed record with key alice and value {"count":8}, and updated total count to 36
+Consumed record with key alice and value {"count":9}, and updated total count to 45
 
 ```
---
+++ kafka-consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using Newtonsoft.Json.Linq;
+
+namespace kafka_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumerConfig = new ConsumerConfig(GetConfig());
+            consumerConfig.GroupId = "kafka-consumer";
+            consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+            consumerConfig.EnableAutoCommit = false;
+            var topic = "first_topic";
+            var cts = new CancellationTokenSource();
+
+            using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(topic);
+                var totalCount = 0;
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        totalCount += JObject.Parse(cr.Message.Value.ToString()).Value<int>("count");
+                        Console.WriteLine($"Consumed record with key {cr.Message.Key} and value {cr.Message.Value}, and updated total count to {totalCount}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static ClientConfig GetConfig()
+        {
+            var config = new ClientConfig();
+            config.BootstrapServers = "localhost:9092";
+            return config;
+        }
+    }
+}
